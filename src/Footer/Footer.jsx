4b345c8f0795c4acee250c23c1e5a52829f3de53
Leandro Nieto <lean-nieto.92@hotmail.com>
2023-02-24 14:32:43 -0300

    Rocio de miel

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
               </a>
             </div> 
             <div className='igContenedor'>
-              <a href="https://api.whatsapp.com/send?phone=1133392396" className='whatsap'> 
+              <a href="https://api.whatsapp.com/send?phone=5491133392396" className='whatsap'> 
                 <i className="bi bi-whatsapp"></i>
               </a>     
             </div>   
