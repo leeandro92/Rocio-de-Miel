c7ae925f18888c8a165978ac27f6e0d757a76dc5
Leandro Nieto <lean-nieto.92@hotmail.com>
2023-02-26 13:29:48 -0300

    rociodemiel

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <section className='home'>
         <Seccion1/> 
         <div style={{backgroundColor:"rgba(255, 192, 203, 0.638)",padding:"10px"}}>     
-        <img src="http://www.pasteleriaartesanal.com/sitio/images/nuevo_icos_ancho4.png" className="img-fluid" alt="..."style={{height:"100px"}}/> 
+        <img src="fotoHome.png" className="img-fluid" alt="..."style={{height:"100px"}}/> 
         <h2 style={{textAlign:"center",fontSize:"40px",padding:"3px",margin:"10px",letterSpacing:"5px"}}>Rocio de miel</h2>
       </div>   
         <Seccion2/>
